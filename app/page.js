c034900327d281ab1d7fa8a3aa193d48e4b3823e
cyamann <cerenyaman321123@gmail.com>
2024-11-25 23:48:05 +0300

    modal eklendi

--- app/page.js
+++ app/page.js
@@ -3,11 +3,12 @@ import { currencyFormatter } from '../app/lib/utils';
 import ExpenseCategoryItem from '@/components/ExpenseCategoryItem';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from "react-chartjs-2";
-
+import { useState } from 'react';
+import Modal from '@/components/Modal';
 // ChartJS modülünü kaydedin
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const DUMMY_DATE = [
+const DUMMY_DATA = [
   {
     id: 1,
     title: "Eğlence",
@@ -29,54 +30,62 @@ const DUMMY_DATE = [
 ];
 
 export default function Home() {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
   return (
-    <main className="container max-w-2xl px-6 py-6 mx-auto">
-      <section className="py-3">
-        <small className="text-gray-400 text-md">
-          Birikim
-        </small>
-        <h2 className="text-4xl font-bold">{currencyFormatter(100)}</h2>
-      </section>
-      <section className="flex items-center gap-2 py-3">
-        <button className="btn btn-primary">+ Harcamalar</button>
-        <button className="btn btn-primary-outline">+ Gelirler</button>
-      </section>
+    <>
+      <Modal show= {modalIsOpen} onClose={setModalIsOpen}>
+        <h3>Hello world</h3>
+      </Modal>
+      
+      <main className="container max-w-2xl px-6 py-6 mx-auto">
+        <section className="py-3">
+          <small className="text-gray-400 text-md">
+            Birikim
+          </small>
+          <h2 className="text-4xl font-bold">{currencyFormatter(100)}</h2>
+        </section>
+        <section className="flex items-center gap-2 py-3">
+          <button onClick={()=> {setModalIsOpen(true)}}   className="btn btn-primary">+ Harcamalar</button>
+          <button className="btn btn-primary-outline">+ Gelirler</button>
+        </section>
 
-      <section className="py-6">
-        <h3>Harcamalarım</h3>
-        <div className="flex flex-col gap-4 mt-6">
-          {DUMMY_DATE.map((expense) => {
-            return (
-              <ExpenseCategoryItem
-                key={expense.id} // Benzersiz key özelliği
-                color={expense.color}
-                title={expense.title}
-                amount={expense.amount}
-              />
-            );
-          })}
-        </div>
-      </section>
+        <section className="py-6">
+          <h3>Harcamalarım</h3>
+          <div className="flex flex-col gap-4 mt-6">
+            {DUMMY_DATA.map((expense) => {
+              return (
+                <ExpenseCategoryItem
+                  key={expense.id}
+                  color={expense.color}
+                  title={expense.title}
+                  amount={expense.amount}
+                />
+              );
+            })}
+          </div>
+        </section>
 
-      <section className="py-6">
-        <h3 className="text-2xl">İstatistikler</h3>
-        <div className='w-1/2 mx-auto'>
-          <Doughnut
-            data={{
-              labels: DUMMY_DATE.map((expense) => expense.title),
-              datasets: [
-                {
-                  label: "Harcamalar",
-                  data: DUMMY_DATE.map((expense) => expense.amount),
-                  backgroundColor: DUMMY_DATE.map((expense) => expense.color),
-                  borderColor: ["#18181b"],
-                  borderWidth: 5,
-                },
-              ],
-            }}
-          />
-        </div>
-      </section>
-    </main>
+        <section className="py-6">
+          <h3 className="text-2xl">İstatistikler</h3>
+          <div className="w-1/2 mx-auto">
+            <Doughnut
+              data={{
+                labels: DUMMY_DATA.map((expense) => expense.title),
+                datasets: [
+                  {
+                    label: "Harcamalar",
+                    data: DUMMY_DATA.map((expense) => expense.amount),
+                    backgroundColor: DUMMY_DATA.map((expense) => expense.color),
+                    borderColor: ["#18181b"],
+                    borderWidth: 5,
+                  },
+                ],
+              }}
+            />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
