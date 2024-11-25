8d7cae77c9bd5aab1d74cf1392187d1e324dc3ac
cyamann <cerenyaman321123@gmail.com>
2024-11-25 22:15:37 +0300

    Nav eklendi

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
-import Image from "next/image";
-
+import Nav from '../components/Navigation';
 export default function Home() {
   return (
-   <h1>hello world</h1>
+   <Nav/>
   );
 }
---
+++ components/Navigation.js
@@ -0,0 +1,25 @@
+import { IoStatsChart } from "react-icons/io5";
+
+function Nav(){
+    return <header className=" container max-w-2xl px-6 py-6 mx-auto">
+    <div className=" flex items-center justify-between">
+
+    
+    <div className="flex flex-row items-center gap-2">
+    <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+      <img className = "object-cover w-full h-full" src="https://thispersondoesnotexist.com/" alt="Profile Image"></img>
+      </div>
+      <name>Hi Ceren</name>
+    </div>
+    <nav className="flex items-center gap-2">
+      <div>
+      <IoStatsChart className="text-2xl"/>
+      </div>
+      <div>
+        <button className="btn btn-danger">Sign Out</button>
+      </div>
+    </nav>
+    </div>
+   </header>
+}
+export default Nav;
